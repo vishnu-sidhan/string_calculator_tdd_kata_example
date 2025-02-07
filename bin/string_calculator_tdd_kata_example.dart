7ef4067b15c376d0434e60cc77643f5538f514e8
vishnusidhan <vsidhan@gmail.com>
2025-02-07 10:07:10 +0530

    first commit

---
+++ bin/string_calculator_tdd_kata_example.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_tdd_kata_example/string_calculator_tdd_kata_example.dart' as string_calculator_tdd_kata_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_tdd_kata_example.calculate()}!');
+}
---
+++ test/string_calculator_tdd_kata_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_tdd_kata_example/string_calculator_tdd_kata_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
