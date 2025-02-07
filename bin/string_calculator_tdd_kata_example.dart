58b9b014c069fd5dd23e1d947c9bd3e98948740d
vishnusidhan <vsidhan@gmail.com>
2025-02-07 12:46:10 +0530

    Handling two numbers in string

--- bin/string_calculator_tdd_kata_example.dart
+++ bin/string_calculator_tdd_kata_example.dart
@@ -4,4 +4,5 @@ void main(List<String> arguments) {
   StringCalculator stringCalculator = StringCalculator();
   print(stringCalculator.add(""));
   print(stringCalculator.add("1"));
+  print(stringCalculator.add("1,2"));
 }
--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,12 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+    int indexOfComma = numbers.indexOf(",");
+    if (indexOfComma != -1) {
+      int value1 = int.parse(numbers.substring(0, indexOfComma));
+      int value2 = int.parse(numbers.substring(indexOfComma + 1));
+      return value1 + value2;
+    }
     return int.parse(numbers);
   }
 }
