d65c549d1ca10c222f9f422bb5603db3cab23b7c
vishnusidhan <vsidhan@gmail.com>
2025-02-08 10:21:01 +0530

    Removed unnecesary code and added comments

--- bin/string_calculator_tdd_kata_example.dart
+++ bin/string_calculator_tdd_kata_example.dart
@@ -3,7 +3,7 @@ import 'package:string_calculator_tdd_kata_example/string_calculator.dart';
 void main(List<String> arguments) {
   StringCalculator stringCalculator = StringCalculator();
   print(stringCalculator.add(""));
-  print(stringCalculator.add("1"));
+  print(stringCalculator.add("1001"));
   print(stringCalculator.add("1,2"));
   print(stringCalculator.add("1,2,21,3"));
   print(stringCalculator.add("1,2\n3,10"));
--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,43 +1,44 @@
 class StringCalculator {
+  static const String commonDelimiter = ",";
+
   int add(String numbers) {
+    // When string is empty.
     if (numbers.isEmpty) return 0;
-    String commonDelimiter = ",";
     List<String> delimiters = [];
-    if (numbers.contains('\n')) {
-      delimiters.add('\n');
-    }
+    // When string contains '\n'.
+    if (numbers.contains('\n')) delimiters.add('\n');
+
+    // When the string has different delimiters than ','.
     if (numbers.startsWith("//")) {
       numbers = numbers.substring(2);
-      bool toRepeat = false;
       do {
-        (String, bool) temp = createDelimitersList(numbers, delimiters);
-        numbers = temp.$1;
-        toRepeat = temp.$2;
-      } while (toRepeat);
+        numbers = addDelimiterToList(numbers, delimiters);
+      } while (numbers[0] != '\n');
     }
+    // Replace '\n' or any other delimiters with ','.
     if (delimiters.isNotEmpty) {
       for (String val in delimiters) {
         numbers = numbers.replaceAll(val, commonDelimiter);
       }
     }
-    if (numbers.contains(commonDelimiter)) {
-      List<String> list = numbers.split(commonDelimiter);
-      int total = 0;
-      List<int> negativeValues = [];
-      for (String val in list) {
-        int num = stringToInt(val);
-        if (num < 0) {
-          negativeValues.add(num);
-          continue;
-        }
-        if (num < 1000) total += num;
+    // Converting the string numbers to list of integers.
+    List<int> values =
+        numbers.split(commonDelimiter).map((e) => stringToInt(e)).toList();
+    int total = 0;
+    List<int> negativeValues = [];
+    // Adding integers values according to conditions.
+    for (int val in values) {
+      if (val < 0) {
+        negativeValues.add(val);
+        continue;
       }
-      if (negativeValues.isNotEmpty) {
-        return throw "Negatives not allowed - ${negativeValues.join(",")}";
-      }
-      return total;
+      if (val < 1000) total += val;
     }
-    return stringToInt(numbers);
+    // Throwing error if negatives values present in string.
+    if (negativeValues.isNotEmpty) {
+      return throw "Negatives not allowed - ${negativeValues.join(",")}";
+    }
+    return total;
   }
 
   int stringToInt(String val) {
@@ -45,7 +46,7 @@ class StringCalculator {
     return int.parse(val);
   }
 
-  (String, bool) createDelimitersList(String numbers, List<String> delimiters) {
+  String addDelimiterToList(String numbers, List<String> delimiters) {
     String temp = numbers[0];
     int count = 1;
     for (int i = 1; i < numbers.length; i++) {
@@ -57,6 +58,6 @@ class StringCalculator {
       }
     }
     delimiters.add(List.generate(count, (i) => temp).join());
-    return (numbers, numbers[0] != '\n');
+    return numbers;
   }
 }
